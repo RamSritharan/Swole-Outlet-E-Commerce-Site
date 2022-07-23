38b5859b411b94ed4668d7053bed2ddc742278a3
RamSritharan <ramkishan1795@gmail.com>
2022-07-23 18:09:44 -0400

    Adding in home page

---
+++ swoleoutlet/main_app/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class Product(models.Model):
+  name = models.CharField(max_length=100)
+  product_type = models.CharField(max_length=100)
+  price = models.IntegerField()
+  quantity_available = models.IntegerField()
+  product_description = models.TextField(max_length=250)
+
+  def __str__(self):
+        return self.name
+
