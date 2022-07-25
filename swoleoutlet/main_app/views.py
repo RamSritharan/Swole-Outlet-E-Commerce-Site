00553369195a478068cc99f5fce1b9cb80d5e5cd
RamSritharan <ramkishan1795@gmail.com>
2022-07-25 10:43:02 -0400

    Adding in sign-up feature

--- swoleoutlet/main_app/views.py
+++ swoleoutlet/main_app/views.py
@@ -4,6 +4,10 @@ from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import ListView, DetailView
 from django.http import HttpResponse
 from .models import Product, Order
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 
 
 # Define the home view
@@ -22,6 +26,8 @@ def products_detail(request, product_id):
     product = Product.objects.get(id=product_id)
     return render(request, 'product/detail.html', { 'product': product  })
 
+
+
 def assoc_order(request, product_id, order_id):
   Product.objects.get(id=product_id).orders.add(order_id)
   return redirect('detail', product_id=product_id)
@@ -31,21 +37,38 @@ def unassoc_order(request, product_id, order_id):
   return redirect('detail', product_id=product_id)
 
 
-
-class OrderList(ListView):
+class OrderList(LoginRequiredMixin, ListView):
   model = Order
 
-class OrderDetail(DetailView):
+class OrderDetail(LoginRequiredMixin, DetailView):
   model = Order
 
-class OrderCreate(CreateView):
+
+class OrderCreate(LoginRequiredMixin, CreateView):
   model = Order
   fields = '__all__'
 
-class OrderUpdate(UpdateView):
+class OrderUpdate(LoginRequiredMixin, UpdateView):
   model = Order
   fields = ['name', 'color']
 
-class OrderDelete(DeleteView):
+class OrderDelete(LoginRequiredMixin, DeleteView):
   model = Order
-  success_url = '/toys/'
+  success_url = '/orders/'
+
+
+def signup(request):
+  error_message = ''
+  if request.method == 'POST':
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      user = form.save()
+      login(request, user)
+      return redirect('products')
+    else:
+      error_message = 'Invalid sign up - try again'
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'registration/signup.html', context)
+
+
